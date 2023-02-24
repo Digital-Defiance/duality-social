4a82e9e40d678a1d9426897d745f56185c1682b9
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2023-02-23 22:19:07 -0800

    acquireTokenSilent test button works

--- packages/duality-social-angular/src/home/home.component.ts
+++ packages/duality-social-angular/src/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
-import { AuthenticationResult, EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
+import { AuthenticationResult, EventMessage, EventType, InteractionStatus, SilentRequest } from '@azure/msal-browser';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -12,6 +12,23 @@ export class HomeComponent implements OnInit {
   loginDisplay = false;
 
   constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
+
+  public async test() {
+    const accountInfo = this.authService.instance.getActiveAccount();
+    if (!accountInfo) {
+      return;
+    }
+    // make a call to the API to test
+    const request: SilentRequest = {
+      scopes: ['User.Read','openid','profile'],
+      account: accountInfo,
+    };
+    await this.authService.instance.acquireTokenSilent(request).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 
   ngOnInit(): void {
     this.msalBroadcastService.msalSubject$
