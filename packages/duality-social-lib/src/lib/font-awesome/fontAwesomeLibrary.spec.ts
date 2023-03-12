c0f42c600f80b3ef20e3fe6016a20324a0ad571c
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2023-03-12 15:39:03 -0700

    better define types, tests, still fail

--- packages/duality-social-lib/src/lib/font-awesome/fontAwesomeLibrary.spec.ts
+++ packages/duality-social-lib/src/lib/font-awesome/fontAwesomeLibrary.spec.ts
@@ -1,35 +1,6 @@
 import { IconDefinition, IconName, IconPack, IconPathData, IconPrefix } from '@fortawesome/fontawesome-common-types';
 import { FontAwesomeLirary } from './fontAwesomeLibrary';
-import { IconNames } from './font-awesome-types';
-
-function makeIconDefinition(iconName: IconName, prefix: IconPrefix = 'far'): IconDefinition {
-    return {
-        iconName: iconName,
-        prefix: prefix,
-        icon: [0, 0, [''], '', ''], // bogus data
-    };
-}
-
-function makeIconPack(icons: IconDefinition[]): IconPack {
-    const result: IconPack = {};
-    for (const icon of icons) {
-        result[icon.iconName] = icon;
-    }
-    return result;
-}
-
-
-function generateIconPack(count: number): IconPack {
-    const icons: IconDefinition[] = [];
-    for (let i = 0; i < count; i++) {
-        // pick a random IconName
-        const iconName: IconName = IconNames[Math.floor(Math.random() * IconNames.length)];
-        const definition = makeIconDefinition(iconName);
-        icons.push(definition);
-    }
-    return makeIconPack(icons);
-}
-
+import { generateIconPack } from './faTestUtil';
 
 describe('FontAwesomeLirary', () => {
     let library: FontAwesomeLirary;
@@ -46,13 +17,11 @@ describe('FontAwesomeLirary', () => {
     });
     it('should add icon definitions to the library', () => {
         const iconPack: IconPack = generateIconPack(2);
-        const icons: IconDefinition[] = [];
         for (const icon of Object.values(iconPack)) {
             library.add(icon);
-            icons.push(icon);
         }
-        expect(library.definitions).toContain(icons[0]);
-        expect(library.definitions).toContain(icons[1]);
+        expect(library.definitions).toContain(iconPack[Object.keys(iconPack)[0]]);
+        expect(library.definitions).toContain(iconPack[Object.keys(iconPack)[1]]);
     });
 
     it('should reset the library', () => {
@@ -70,13 +39,11 @@ describe('FontAwesomeLirary', () => {
 
     it('should search for icon definitions', () => {
         const iconPack: IconPack = generateIconPack(3);
-        expect(library.definitions.length).toEqual(3);
-        const icons: IconDefinition[] = [];
+        const icons = Object.values(library.definitions);
+        expect(icons.length).toEqual(3);
         for (const icon of Object.values(iconPack)) {
             library.add(icon);
-            icons.push(icon);
         }
-
         expect(library.search((icons[0].iconName as string).substring(0, 3))).toContain(icons[0]);
         expect(library.search((icons[1].iconName as string).substring(0, 3))).toContain(icons[1]);
         expect(library.search((icons[2].iconName as string).substring(0, 3))).toContain(icons[2]);
