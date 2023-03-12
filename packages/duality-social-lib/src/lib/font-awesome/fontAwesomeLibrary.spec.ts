12eac2c4231282992835f72f521149d9c24855a3
Freddie Mercurial (Jessica M.) <jessica@brightchain.org>
2023-03-12 14:55:29 -0700

    trying to corral fontawesome

--- packages/duality-social-lib/src/lib/font-awesome/fontAwesomeLibrary.spec.ts
+++ packages/duality-social-lib/src/lib/font-awesome/fontAwesomeLibrary.spec.ts
@@ -1,52 +1,84 @@
-import { IconDefinition, IconPack } from '@fortawesome/fontawesome-svg-core';
+import { IconDefinition, IconName, IconPack, IconPathData, IconPrefix } from '@fortawesome/fontawesome-common-types';
 import { FontAwesomeLirary } from './fontAwesomeLibrary';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { IconNames } from './font-awesome-types';
+
+function makeIconDefinition(iconName: IconName, prefix: IconPrefix = 'far'): IconDefinition {
+    return {
+        iconName: iconName,
+        prefix: prefix,
+        icon: [0, 0, [''], '', ''], // bogus data
+    };
+}
+
+function makeIconPack(icons: IconDefinition[]): IconPack {
+    const result: IconPack = {};
+    for (const icon of icons) {
+        result[icon.iconName] = icon;
+    }
+    return result;
+}
+
+
+function generateIconPack(count: number): IconPack {
+    const icons: IconDefinition[] = [];
+    for (let i = 0; i < count; i++) {
+        // pick a random IconName
+        const iconName: IconName = IconNames[Math.floor(Math.random() * IconNames.length)];
+        const definition = makeIconDefinition(iconName);
+        icons.push(definition);
+    }
+    return makeIconPack(icons);
+}
+
 
 describe('FontAwesomeLirary', () => {
-  let library: FontAwesomeLirary;
+    let library: FontAwesomeLirary;
 
-  beforeEach(() => {
-    library = new FontAwesomeLirary();
-  });
+    beforeEach(() => {
+        library = new FontAwesomeLirary();
+    });
+    it('should add icon packs to the library', () => {
+        const iconPack: IconPack = generateIconPack(2);
+        expect(Object.keys(iconPack).length).toEqual(2);
+        library.add(iconPack);
+        expect(library.definitions).toContain(iconPack[Object.keys(iconPack)[0]]);
+        expect(library.definitions).toContain(iconPack[Object.keys(iconPack)[1]]);
+    });
+    it('should add icon definitions to the library', () => {
+        const iconPack: IconPack = generateIconPack(2);
+        const icons: IconDefinition[] = [];
+        for (const icon of Object.values(iconPack)) {
+            library.add(icon);
+            icons.push(icon);
+        }
+        expect(library.definitions).toContain(icons[0]);
+        expect(library.definitions).toContain(icons[1]);
+    });
 
-  it('should add icon definitions to the library', () => {
-    const icon1: IconDefinition = fas.facebook;
-    const icon2: IconDefinition = fas.twitter;
-    const iconPack: IconPack = {
-        tiktok: fas.tiktok,
-        instagram: fas.instagram,
-    };
+    it('should reset the library', () => {
+        const iconPack: IconPack = generateIconPack(1);
+        const icons: IconDefinition[] = [];
+        for (const icon of Object.values(iconPack)) {
+            library.add(icon);
+            icons.push(icon);
+        }
+        expect(library.definitions).toContain(icons[0]);
 
-    library.add(icon1, icon2, iconPack);
+        library.reset();
+        expect(library.definitions).toEqual([]);
+    });
 
-    expect(library.definitions).toContain(icon1);
-    expect(library.definitions).toContain(icon2);
-    expect(library.definitions).toContain(iconPack.tiktok);
-    expect(library.definitions).toContain(iconPack.instagram);
-  });
+    it('should search for icon definitions', () => {
+        const iconPack: IconPack = generateIconPack(3);
+        expect(library.definitions.length).toEqual(3);
+        const icons: IconDefinition[] = [];
+        for (const icon of Object.values(iconPack)) {
+            library.add(icon);
+            icons.push(icon);
+        }
 
-  it('should reset the library', () => {
-    const icon1: IconDefinition = fas.facebook;
-    library.add(icon1);
-    expect(library.definitions).toContain(icon1);
-
-    library.reset();
-    expect(library.definitions).toEqual([]);
-  });
-
-  it('should search for icon definitions', () => {
-    const icon1: IconDefinition = fas.facebook;
-    const icon2: IconDefinition = fas.twitter;
-    const iconPack: IconPack = {
-      faIcon3: fas.linkedin,
-      faIcon4: fas.instagram,
-    };
-
-    library.add(icon1, icon2, iconPack);
-
-    expect(library.search('face')).toContain(icon1);
-    expect(library.search('tw')).toContain(icon2);
-    expect(library.search('link')).toContain(iconPack.faIcon3);
-    expect(library.search('volkswagen')).toEqual([]);
-  });
+        expect(library.search((icons[0].iconName as string).substring(0, 3))).toContain(icons[0]);
+        expect(library.search((icons[1].iconName as string).substring(0, 3))).toContain(icons[1]);
+        expect(library.search((icons[2].iconName as string).substring(0, 3))).toContain(icons[2]);
+    });
 });
