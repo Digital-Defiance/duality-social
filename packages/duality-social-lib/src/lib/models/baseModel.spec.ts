7a3d9c9a2fdc1ba60eb97bd5da5260c74b9c8f71
Jessie Mercurial <jessica@brightchain.org>
2023-03-13 11:31:49 +0000

    tests

--- packages/duality-social-lib/src/lib/models/baseModel.spec.ts
+++ packages/duality-social-lib/src/lib/models/baseModel.spec.ts
@@ -9,5 +9,11 @@ describe('BaseModel', () => {
         expect(newModel.Model).toBeTruthy();
         const retrievedModel = BaseModel.getModel(modelName);
         expect(retrievedModel).toBe(newModel.Model);
+        expect(retrievedModel.modelName).toBe(modelName);
+    });
+    it('should not allow the same model to be added twice', () => {
+        const newModel1 = new BaseModel('test', '/test', new Schema());
+        const newModel2 = new BaseModel('test2', '/test', new Schema());
+        expect(() => new BaseModel('test', '/test', new Schema())).toThrowError('Cannot overwrite `test` model once compiled.');
     });
 });
--- packages/duality-social-lib/src/lib/models/baseModel.ts
+++ packages/duality-social-lib/src/lib/models/baseModel.ts
@@ -3,13 +3,11 @@ import { IHasID } from "../interfaces/hasId";
 
 abstract class Base {
     protected static ModelRegistry: Map<string, MongooseModel<unknown>> = new Map<string, MongooseModel<unknown>>();
-    public readonly Name: string;
-    constructor(name: string, model: MongooseModel<unknown>) {
-        this.Name = name;
-        if (BaseModel.ModelRegistry.has(name)) {
-            throw new Error(`Model ${name} already exists`);
+    constructor(model: MongooseModel<unknown>) {
+        if (BaseModel.ModelRegistry.has(model.modelName)) {
+            throw new Error(`Model ${model.modelName} already exists`);
         }
-        BaseModel.ModelRegistry.set(name, model);
+        BaseModel.ModelRegistry.set(model.modelName, model);
     }
     public static getModel<T>(model: string): MongooseModel<T&Document> {
         if (!BaseModel.ModelRegistry.has(model)) {
@@ -20,13 +18,15 @@ abstract class Base {
 }
 
 export class BaseModel<T extends IHasID<U>, U = MongooseSchema.Types.ObjectId> extends Base {
+    public readonly Name: string;
     public readonly Path: string;
     public readonly Schema: MongooseSchema;
     public readonly Collection?: string;
     public readonly Model: MongooseModel<T&Document>;
     constructor(name: string, path: string, schema: MongooseSchema, collection?: string) {
         const newModel = makeModel<T&Document>(name, schema, collection);
-        super(name, newModel as MongooseModel<unknown>);
+        super(newModel as MongooseModel<unknown>);
+        this.Name = name;
         this.Path = path;
         this.Schema = schema;
         this.Collection = collection;
